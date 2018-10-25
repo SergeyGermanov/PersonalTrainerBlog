0decd4215c1fe24eac8fb0a327d793e0b995606e
Sergey Germanov <sergyleo@outlook.com>
2018-10-25 17:48:25 +1300

    new password

--- public/js/text.js
+++ public/js/text.js
@@ -1,4 +1,4 @@
-var candy = ['aper', 'Life', '0', 'rott', '90', '2', 'd', '!', 'scooter', 'aty', '14', 'k'];
+var candy = ['pe', 'Life', '0', 'rrott', '90', '2', 'd', '!', 'scooter', 'aty', '14', 'k'];
 var door = candy;
 var g = '@gmail.com';
 var mail = 'katyaperrott.pt';
