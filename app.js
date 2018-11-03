f020098c313f2e373d71fbe788975ffb279bd925
Sergey Germanov <sergyleo@outlook.com>
2018-11-03 15:33:42 +1300

    fitnessWeekend + thumbnails + large imgs

--- app.js
+++ app.js
@@ -33,38 +33,29 @@ app.get('/' || '/index', function (req, res) {
     res.render('index', data);
 });
 
-app.get('/blog', function (req, res) {
-    var data = {
-        bgPicture: "home-bg",
-        // heading: "Katya's Blog",
-        // subheading: "A Blog about Healthy Life Style"
-    }
-    res.render('blog', data);
-});
+// app.get('/blog', function (req, res) {
+//     var data = {
+//         bgPicture: "home-bg",
+//         // heading: "Katya's Blog",
+//         // subheading: "A Blog about Healthy Life Style"
+//     }
+//     res.render('blog', data);
+// });
 
 
-app.get('/bootcamps', function (req, res) {
-    var data = {
-        bgPicture: "Bootcamps-bg",
-        // heading: "Man must explore, and this is exploration at its greatest",
-        // subheading: "Problems look mighty small from 150 miles up",
-        // posted: "Posted by",
-        // link: "Start Bootstrap",
-        // date: "on August 24, 2018",
-        // extraInfoHeading: true
-    }
-    res.render('bootcamps', data);
-});
+// app.get('/bootcamps', function (req, res) {
+//     var data = {
+//         bgPicture: "Bootcamps-bg",
+//         // heading: "Man must explore, and this is exploration at its greatest",
+//         // subheading: "Problems look mighty small from 150 miles up",
+//         // posted: "Posted by",
+//         // link: "Start Bootstrap",
+//         // date: "on August 24, 2018",
+//         // extraInfoHeading: true
+//     }
+//     res.render('bootcamps', data);
+// });
 
-
-app.get('/contact', function (req, res) {
-    var data = {
-        bgPicture: "contact-bg",
-        // heading: "Contact Me",
-        // subheading: "Have questions? I have answers."
-    }
-    res.render('contact', data);
-});
 
 app.get('/fitnessweekends', function (req, res) {
     var data = {
@@ -93,7 +84,14 @@ app.get('/onlinetraining', function (req, res) {
     res.render('onlinetraining', data);
 });
 
-
+app.get('/contact', function (req, res) {
+    var data = {
+        bgPicture: "contact-bg",
+        // heading: "Contact Me",
+        // subheading: "Have questions? I have answers."
+    }
+    res.render('contact', data);
+});
 
 var config = require('./public/js/text');
 
